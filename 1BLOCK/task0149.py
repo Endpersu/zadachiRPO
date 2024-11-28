af68c9193efe72cd2b53f6f6a325a290e908f696
Endpersu <67841931+Endpersu@users.noreply.github.com>
2024-11-28 08:50:32 +0000

    0149

---
+++ 1BLOCK/task0149.py
@@ -0,0 +1,6 @@
+## синус а * косинус б + косинус а * синус б
+from math import sin, cos, radians
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+otvet = round(sin(radians(a)), 2) * round(cos(radians(b)), 2) + round(cos(radians(a)), 2) * round(sin(radians(b)), 2)
+print(f"Ваш ответ: {otvet}")
