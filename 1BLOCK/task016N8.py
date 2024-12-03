b7f78e7b75d7de43a1bfddcd1ece56e0ba81aa35
Endpersu <67841931+Endpersu@users.noreply.github.com>
2024-12-03 08:50:46 +0000

    16

--- 1BLOCK/task016N8.py
+++
@@ -1,5 +0,0 @@
-from math import sin, radians
-a = int(input("Введите первое число: "))
-b = int(input("Введите второе число: "))
-otvet = a / round(sin(radians(b)), 2)
-print(f"Ваш ответ: {otvet}")
--- 1BLOCK/task016N9.py
+++
@@ -1,6 +0,0 @@
-from math import sin, radians
-a = int(input("Введите первое число: "))
-b = int(input("Введите второе число: "))
-x = int(input("Введите третье число: "))
-otvet = 1/2 * a * b * round(sin(radians(x)), 2)
-print(f"Ваш ответ: {otvet}")
