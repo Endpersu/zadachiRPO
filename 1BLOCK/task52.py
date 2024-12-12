046ed1162f0c6520e430608f7afc157753f28d7f
Endpersu <67841931+Endpersu@users.noreply.github.com>
2024-12-12 10:03:44 +0000

    52 u 55

--- 1BLOCK/task52.py
+++ 1BLOCK/task52.py
@@ -1,15 +1,8 @@
-def calculate_total_cost(candy_price, cookie_price, apple_price, x, y, z):
-    total_cost = (candy_price * x) + (cookie_price * y) + (apple_price * z)
-    return total_cost
-
-
-candy_price = 500  
-cookie_price = 300  
-apple_price = 100  
-
-x = 2  
-y = 3  
-z = 5  
-
-total = calculate_total_cost(candy_price, cookie_price, apple_price, x, y, z)
-print(f"Общая стоимость покупки: {total} рублей")
+x = int(input("Введите сколько килограмм конфет купили: "))
+y = int(input("Введите сколько килограмм печенья купили: "))
+z = int(input("Введите сколько килограмм яблок купили: "))
+s1 = int(input("Введите стоимость конфет: "))
+s2 = int(input("Введите стоимость печенья: "))
+s3 = int(input("Введите стоимость яблок: "))
+all = (x * s1) + (y * s2) + (z * s3)
+print("Всего вы купили", all, "рублей продуктов")
