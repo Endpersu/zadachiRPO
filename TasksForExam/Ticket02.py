b054d8bb8effa667311dee4b3a3c39818a5c75e9
Luch3r <luchkin_yrok@mail.ru>
2025-01-17 11:44:29 +0300

    4block

---
+++ TasksForExam/Ticket02.py
@@ -0,0 +1,13 @@
+from random import randint
+
+
+def triple_characters(s):
+    return ''.join([char * 3 for char in s])
+
+
+def 
+
+
+original_string = 'Привет!'
+tripled_string = triple_characters(original_string)
+print(tripled_string)
