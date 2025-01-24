237d3eff00a6cbc05abab9db348e5ddc86346321
Luch3r <luchkin_yrok@mail.ru>
2025-01-24 15:47:27 +0300

    update

--- TasksForExamPython/Ticket12.py
+++ TasksForExamPython/Ticket12.py
@@ -1,6 +1,6 @@
 def main():
-    user_input=input("Введите что-то: ")
-   
+    user_input = input("Введите что-то: ")
+
     try:
        float_value = float(user_input)
        if '.' in user_input or 'e' in user_input.lower():
@@ -9,10 +9,10 @@ def main():
            print(f"Тип:{type(int(float_value)).__name__}(целое число)")
     except ValueError:
         if user_input.lower() in ['true', 'false']:
-            print(f"Тип:{type(user_input.lower() == 'true').__name__}(логическое значение)")
+            print(f"Тип:{type(user_input.lower() == 'true').__name__}(
+                  логическое значение)")
         else:
             print(f"Тип: {type(user_input).__name__}")
-            
 
 
 main()
